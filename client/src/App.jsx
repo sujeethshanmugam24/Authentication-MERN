b6832e83b573f73783182675abd8eccaa02ceb3a
sujeethshanmugam24 <21IT268@stjosephs.ac.in>
2024-02-21 19:51:19 +0530

    header completed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,23 @@
-import React from 'react'
+import { BrowserRouter,Routes,Route } from 'react-router-dom'
+import Home from './pages/Home';
+import About from './pages/About';
+import Signin from './pages/Signin';
+import Profile from './pages/Profile';
+import Signup from './pages/Signup';
+import Header from './Components/Header';
+
 
 export default function App() {
   return (
-    <div>App</div>
+    <BrowserRouter>
+    <Header/>
+    <Routes>
+      <Route path='/' element={<Home/>}/>
+      <Route path='/About' element={<About/>}/>
+      <Route path='/Signin' element={<Signin/>}/>
+      <Route path='/Signup' element={<Signup/>}/>
+      <Route path='/Profile' element={<Profile/>}/>
+    </Routes>
+    </BrowserRouter>
   )
 }
---
+++ client/src/Components/Header.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+export default function Header() {
+  return (
+    <div className="bg-slate-600">
+        <div className='flex justify-between p-3 items-center max-w-6xl mx-auto'>
+            <Link to={'/'}><h1 className="font-bold">Authenticaion</h1></Link>
+            <ul className='flex gap-4'>
+                <Link to={'/'}><li>Home</li></Link>
+                <Link to={'/About'}><li>About</li></Link>
+                <Link to={'Signin'}> <li>Signin</li></Link>
+            </ul>
+        </div>
+
+    </div>
+  )
+}
