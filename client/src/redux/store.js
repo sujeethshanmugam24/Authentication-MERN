3aa984aacc033f541f439513a53cf28e5eba5d1d
sujeethshanmugam24 <21IT268@stjosephs.ac.in>
2024-02-25 10:45:34 +0530

    added redux fun

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,9 +1,22 @@
- import {configureStore} from '@reduxjs/toolkit';
+ import {combineReducers, configureStore} from '@reduxjs/toolkit';
 import userreducer from './user/userslice';
+import {persistReducer,persistStore} from 'redux-persist';
+import storage from 'redux-persist/lib/storage'
 
+
+const rootreducer=combineReducers({user:userreducer});
+
+const persistconfig={
+    key:'root',
+    version:1,
+    storage,
+}
+const persistedreducer=persistReducer(persistconfig,rootreducer);
  export const store =configureStore({
-    reducer:{user:userreducer},
+    reducer:persistedreducer,
     middleware:(getDefaultMiddleware)=>getDefaultMiddleware({
         serializableCheck:false,
     })
  })
+
+ export const persistor=persistStore(store);
